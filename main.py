1b0060bc2085eb211cf6911b14067de3681c0b4e
beelzebul-hub1 <stephendavis664+1@gmail.com>
2026-07-27 12:38:41 -0500

    Update streamers from dashboard

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ threading.Thread(target=export_points_loop, daemon=True).start()
 # ---------------- RUN -----------------
 twitch_miner.mine([
         Streamer("shayph"),
-        Streamer("copieburger"),
+        Streamer("sirdapper"),
         Streamer("waffletrades_"),
     ],
     followers=False,
